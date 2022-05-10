7d8c7955c1c1b74e8dbf5c47d23bc287129343e3
pigeonjim <pigeonjim@gmail.com>
2022-05-10 22:11:49 +0100

    Updating data classes

--- src/main/java/com/calendar/AllData.java
+++ src/main/java/com/calendar/AllData.java
@@ -5,10 +5,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class AllData {
-    private HashMap<LocalDate, ArrayList<String>> allData;
-
-    public AllData(){
+    private HashMap<LocalDate, DayData> allData;
+    private DateLogic dLogic;
+    public AllData(DateLogic dLogic){
         allData = new HashMap<>();
+        this.dLogic = dLogic;
+    }
+
+    public void addNewDayData(LocalDate date, String text){
+        if (!allData.containsKey(date)) {
+            allData.put(date, new DayData(dLogic));
+        }
+        allData.get(date).addText(text);
+    }
+
+    public void deleteDayData(LocalDate date, String text){
+        if(allData.containsKey(date)){
+            allData.get(date).removeText(text);
+        }
+    }
+
+    public void deleteAllDataFromDay(LocalDate date){
+        if(allData.containsKey(date)){
+            allData.remove(date);
+        }
     }
 
 
