4e14f114a3d7fb0878764ebc241209324c0d2a4d
pigeonjim <pigeonjim@gmail.com>
2022-05-12 08:32:59 +0100

    Updating POM and testing

---
+++ src/main/java/com/calendar/DataIO.java
@@ -0,0 +1,13 @@
+package com.calendar;
+
+public class DataIO {
+
+    AllData allData;
+
+    public DataIO(AllData allData){
+        this.allData = allData;
+    }
+
+
+
+}
---
+++ src/main/java/com/calendar/Launcher.java
@@ -0,0 +1,7 @@
+package com.calendar;
+
+public class Launcher {
+    public static void main(String[] args){
+        CalendarApplication.main(args);
+    }
+}
